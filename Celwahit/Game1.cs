622e740c985d4caa1e29073f32def67658f71cfa
Dries Leyers <52953642+DriesLeyers@users.noreply.github.com>
2021-11-16 17:10:31 +0100

    Update Game1.cs

--- Celwahit/Game1.cs
+++ Celwahit/Game1.cs
@@ -11,6 +11,9 @@ namespace Celwahit
 
         private Texture2D playerLegs;
 
+        private Rectangle partRectangleLegs;
+        private int moveRectangleLegs_X = 32;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -21,6 +24,7 @@ namespace Celwahit
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            partRectangleLegs = new Rectangle(moveRectangleLegs_X, 23, 32, 23);
 
             base.Initialize();
         }
@@ -47,9 +51,16 @@ namespace Celwahit
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
+
+            _spriteBatch.Draw(playerLegs, new Vector2(0, 10), partRectangleLegs, Color.White);
+
             _spriteBatch.End();
 
-            // TODO: Add your drawing code here
+            moveRectangleLegs_X += 32;
+            if (moveRectangleLegs_X > 384)
+                moveRectangleLegs_X = 0;
+
+            partRectangleLegs.X = moveRectangleLegs_X;
 
             base.Draw(gameTime);
         }

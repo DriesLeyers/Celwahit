27657bdc0504092a062846cdb2eb521f3ff3fd6c
Dries Leyers <dries@dipu.com>
2021-11-23 11:42:47 +0100

    Startscherm

--- Celwahit/Game1.cs
+++ Celwahit/Game1.cs
@@ -2,8 +2,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System;
-
+using System.Threading;
 
 namespace Celwahit
 {
@@ -18,7 +17,22 @@ namespace Celwahit
         Texture2D idlePlayerBody;
         Texture2D idlePlayerLegs;
 
+        private Texture2D startButton;
+        private Vector2 startButtonPosition;
+
+        private Thread backgroundThread;
+        MouseState mouseState;
+        MouseState previousMouseState;
+
         Player player;
+
+        GameState gameState;
+        enum GameState
+        {
+            StartMenu,
+            Playing,
+            Paused
+        }
 
         public Game1()
         {
@@ -29,14 +43,28 @@ namespace Celwahit
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            mouseState = Mouse.GetState();
+            previousMouseState = mouseState;
 
             base.Initialize();
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+            _graphics.ApplyChanges();
+
+            IsMouseVisible = true;
+            LoadGame();
+
+            startButtonPosition = new Vector2(450, 554);
+            gameState = GameState.StartMenu;
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            startButton = Content.Load<Texture2D>("startscherm");
+
             walkingPlayerLegs = Content.Load<Texture2D>("Player/Fiolina_Bot_Walking");
             walkingPlayerBody = Content.Load<Texture2D>("Player/Fiolina_Top_Walking");
 
@@ -48,7 +76,7 @@ namespace Celwahit
 
         private void InitializeGameObjects()
         {
-            player = new Player(walkingPlayerBody, walkingPlayerLegs, idlePlayerBody,idlePlayerLegs);
+            player = new Player(walkingPlayerBody, walkingPlayerLegs, idlePlayerBody, idlePlayerLegs);
         }
 
         protected override void Update(GameTime gameTime)
@@ -58,20 +86,63 @@ namespace Celwahit
 
             player.Update(gameTime);
 
+            mouseState = Mouse.GetState();
+
+            KeyboardState keyboardState = Keyboard.GetState();
+
+            if (gameState == GameState.StartMenu && keyboardState.IsKeyDown(Keys.Enter))
+            {
+                gameState = GameState.Playing;
+            }
+
+            if (previousMouseState.LeftButton == ButtonState.Pressed && mouseState.LeftButton == ButtonState.Released && gameState == GameState.StartMenu)
+            {
+                MouseClicked(mouseState.X, mouseState.Y);
+            }
+
+            previousMouseState = mouseState;
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             _spriteBatch.Begin();
 
-            player.Draw(_spriteBatch, gameTime);
+            if (gameState == GameState.StartMenu)
+            {
+                _spriteBatch.Draw(startButton, new Vector2(0, 0), Color.White);
+            }
+
+            if (gameState == GameState.Playing)
+            {
+                player.Draw(_spriteBatch, gameTime);
+            }
 
             _spriteBatch.End();
-           
+
             base.Draw(gameTime);
+        }
+
+        void MouseClicked(int x, int y)
+        {
+            Rectangle mouseClickRect = new Rectangle(x, y, 10, 10);
+
+            if (gameState == GameState.StartMenu)
+            {
+                Rectangle startButtonRect = new Rectangle((int)startButtonPosition.X, (int)startButtonPosition.Y, 375, 100);
+
+                if (mouseClickRect.Intersects(startButtonRect))
+                {
+                    gameState = GameState.Playing;
+                }
+            }
+        }
+
+        void LoadGame()
+        {
+
         }
     }
 }

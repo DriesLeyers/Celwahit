bad40a76643608b809320ed9bf632bde8b3ccf5f
CedricMartell <cedricmrtl@gmail.com>
2021-11-29 17:46:57 +0100

    Update GameSettings.cs

--- Celwahit/GameSettings.cs
+++ Celwahit/GameSettings.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Celwahit.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Celwahit
 {
-    class GameSettings
+    class GameSettings : IGameSettings
     {
+        public GraphicsDevice GraphicsDevice { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public GraphicsDeviceManager GraphicsManager { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public SpriteBatch SpriteBatch { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int ScreenWidth;
+        public int ScreenHeight;
+
+        public GameSettings(GraphicsDevice GD, GraphicsDeviceManager GM, SpriteBatch SB)
+        {
+            GraphicsDevice = GD;
+            GraphicsManager = GM;
+            SpriteBatch = SB;
+
+            
+        }
     }
 }

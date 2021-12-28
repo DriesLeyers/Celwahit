a80c23a5524d43b61a54aafd9fcdbca7db44d827
CedricMartell <cedricmrtl@gmail.com>
2021-12-28 17:05:33 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/DriesLeyers/Celwahit""
    This reverts commit f941c2f663fd774be70053295fc9cf676dca6a3d.

---
+++ Celwahit/StatePattern/MenuState.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Celwahit.Scenes
+{
+    public class MenuState : SceneState
+    {
+        StartScreen startScreen;
+
+        GameSettings gameSettings;
+
+        MouseState mouseState;
+        MouseState previousMouseState;
+
+        private Texture2D startButton;
+
+        public MenuState(Game1 game, GraphicsDeviceManager graphics, SpriteBatch spriteBatch) : base(game, graphics, spriteBatch)
+        {
+            LoadContent();
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            startScreen.DrawVectorStartButton(startButton, _spriteBatch);
+        }
+
+        public override void Initialize()
+        {
+            
+        }
+
+        public override void LoadContent()
+        {
+            startButton = Content.Load<Texture2D>("startscherm");
+
+            gameSettings = new GameSettings(_graphics);
+
+            mouseState = Mouse.GetState();
+            previousMouseState = mouseState;
+
+            startScreen = new StartScreen(gameSettings);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            mouseState = Mouse.GetState();
+
+            if (startScreen.CheckIfWantToPlay(previousMouseState))
+            {
+                Game1.ChangeSceneState(new LvlOneState(Game1, _graphics, _spriteBatch));
+            }
+
+            previousMouseState = mouseState;
+        }
+    }
+}
